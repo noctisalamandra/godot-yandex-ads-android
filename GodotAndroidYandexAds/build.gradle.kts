ab3f0b0e74fd6fcc39a2d978ba228a852ed354dd
Yoshi <dleonavichus@mail.ru>
2023-12-29 21:37:01 +0300

    godot 4.2

--- GodotAndroidYandexAds/build.gradle.kts
+++ GodotAndroidYandexAds/build.gradle.kts
@@ -30,10 +30,11 @@ android {
 
 dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.google.android.material:material:1.11.0")
     compileOnly(fileTree(mapOf("dir" to "libs", "include" to listOf("*.aar"))))
-    implementation("com.yandex.android:mobileads:6.2.0")        // Yandex mobile ads
-    implementation("io.appmetrica.analytics:analytics:6.0.0")    // Yandex metrics
+    implementation("com.yandex.android:mobileads:6.3.0")        // Yandex mobile ads
+    implementation("io.appmetrica.analytics:analytics:6.0.0")   // Yandex metrics
+    implementation("org.godotengine:godot:4.2.1.stable")        // Godot
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
