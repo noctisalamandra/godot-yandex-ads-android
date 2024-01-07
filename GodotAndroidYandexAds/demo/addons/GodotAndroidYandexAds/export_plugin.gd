aa94be289db7e4bb72955f044e7a1632e210bfe4
Yoshi <dleonavichus@mail.ru>
2024-01-07 18:16:39 +0300

    fix export_plugin

---
+++ GodotAndroidYandexAds/demo/addons/GodotAndroidYandexAds/export_plugin.gd
@@ -0,0 +1,31 @@
+@tool
+extends EditorPlugin
+
+var export_plugin : AndroidExportPlugin
+
+func _enter_tree():
+	export_plugin = AndroidExportPlugin.new()
+	add_export_plugin(export_plugin)
+
+
+func _exit_tree():
+	remove_export_plugin(export_plugin)
+	export_plugin = null
+
+
+class AndroidExportPlugin extends EditorExportPlugin:
+	var _plugin_name = "GodotAndroidYandexAds"
+
+	func _supports_platform(platform):
+		if platform is EditorExportPlatformAndroid:
+			return true
+		return false
+
+	func _get_android_libraries(platform, debug):
+		if debug:
+			return PackedStringArray([_plugin_name + "/bin/debug/" + _plugin_name + "-debug.aar"])
+		else:
+			return PackedStringArray([_plugin_name + "/bin/release/" + _plugin_name + "-release.aar"])
+
+	func _get_name():
+		return _plugin_name
